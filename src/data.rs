6359a7d51dc271eb64aa75bb632dc89143b83879
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2023-01-24 03:15:37 -0300

    refactor

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,5 @@
 use std::cmp::Ordering;
 use std::collections::HashSet;
-use std::ffi::OsStr;
-use std::fs::DirEntry;
 use std::path::PathBuf;
 
 use anyhow::{bail, Result};
@@ -9,16 +7,7 @@ use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::{
-    format_text,
-    helpers::{read_dir_entry_data, write_data},
-    traits::{CourseAssociated, Hashable, WithImage},
-    RawCourseData,
-};
-use crate::{
-    raw_data::{RawQuestionData, RawQuestionOptionData},
-    RawCourseEvaluationData,
-};
+use crate::{format_text, traits::Hashable};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct CourseData {
@@ -39,73 +28,40 @@ pub struct CourseData {
 }
 
 impl CourseData {
-    pub fn new(key: String, raw: RawCourseData) -> Self {
-        let questions: Vec<QuestionData> = raw.questions.into_iter().map(Into::into).collect();
-        let evaluations: Vec<CourseEvaluationData> = raw
-            .evaluations
-            .into_iter()
-            .map(CourseEvaluationData::from)
-            .enumerate()
-            .map(|(index, mut evaluation)| {
-                evaluation.order.replace(index as i16 + 1);
-
-                evaluation
-            })
-            .collect();
-
+    pub fn new(
+        key: String,
+        name: String,
+        short_name: String,
+        tags: Vec<String>,
+        image_file_name: Option<PathBuf>,
+        year: Option<i16>,
+        order: Option<i16>,
+        questions: Vec<QuestionData>,
+        evaluations: Vec<CourseEvaluationData>,
+    ) -> Self {
         Self {
             key,
-            name: raw.name,
-            short_name: raw.short_name,
-            tags: raw.tags,
-            image_file_name: raw.image,
-            year: raw.year,
-            order: raw.order,
+            name,
+            short_name,
+            tags,
+            image_file_name,
+            year,
+            order,
             questions,
             evaluations,
             hash: Default::default(),
         }
     }
 
-    pub async fn load_and_write_formatted(
-        dir_entry: DirEntry,
-        mut images_path: PathBuf,
-    ) -> Result<Self> {
-        let path = dir_entry.path();
-        let mut data = Self::load(path.clone(), dir_entry)?;
-        images_path.push(data.key.clone());
+    pub fn process(&mut self) -> Result<()> {
+        self.clean();
+        self.format();
+        self.sort();
+        self.deduplicate();
+        self.check()?;
+        self.set_hash();
 
-        data.clean();
-        data.format(images_path).await?;
-        data.sort();
-        data.deduplicate();
-        data.check()?;
-
-        data.set_data();
-
-        data.clone().write(path)?;
-
-        Ok(data)
-    }
-
-    pub fn load(path: PathBuf, dir_entry: DirEntry) -> Result<Self> {
-        let raw_data = read_dir_entry_data(dir_entry)?;
-
-        let key = path
-            .file_stem()
-            .and_then(OsStr::to_str)
-            .expect("invalid file name")
-            .to_owned();
-        let raw_course_data = RawCourseData::from_slice(&raw_data[..])?;
-
-        Ok(Self::new(key, raw_course_data))
-    }
-
-    pub fn write(self, path: PathBuf) -> Result<()> {
-        let raw = self.into();
-        let raw_data = serde_json::to_string_pretty::<RawCourseData>(&raw)?;
-
-        write_data(path, raw_data)
+        Ok(())
     }
 
     fn sort(&mut self) {
@@ -148,50 +104,10 @@ impl CourseData {
         Ok(())
     }
 
-    fn set_data(&mut self) {
+    fn format(&mut self) {
         for question in &mut self.questions {
-            question.set_data(self.key.clone());
+            question.format();
         }
-
-        for evaluation in &mut self.evaluations {
-            evaluation.set_data(self.key.clone());
-        }
-
-        self.set_hash();
-    }
-
-    async fn format(&mut self, images_path: PathBuf) -> Result<()> {
-        self.format_image(images_path.clone()).await?;
-
-        for question in &mut self.questions {
-            question.format(images_path.clone()).await?;
-        }
-
-        Ok(())
-    }
-}
-
-impl CourseAssociated for CourseData {
-    fn course_key(&self) -> &str {
-        &self.key
-    }
-
-    fn set_course_key(&mut self, course_key: String) {
-        self.key = course_key;
-    }
-}
-
-impl WithImage for CourseData {
-    fn current_image_file_name(&self) -> Option<&PathBuf> {
-        self.image_file_name.as_ref()
-    }
-
-    fn canonical_image_file_name(&self) -> String {
-        self.key.clone()
-    }
-
-    fn replace_image_file_name(&mut self, new_file_name: PathBuf) {
-        self.image_file_name.replace(new_file_name);
     }
 }
 
@@ -231,6 +147,14 @@ impl Hashable for CourseData {
     }
 
     fn set_hash(&mut self) {
+        for question in &mut self.questions {
+            question.set_hash();
+        }
+
+        for evaluation in &mut self.evaluations {
+            evaluation.set_hash();
+        }
+
         self.hash = self.hash_data();
     }
 }
@@ -239,7 +163,7 @@ impl Hashable for CourseData {
 pub struct QuestionData {
     pub id: Uuid,
 
-    pub course_key: Option<String>,
+    pub course_key: String,
     pub evaluation: String,
     pub source: String,
     pub asked_at: Option<NaiveDate>,
@@ -252,8 +176,9 @@ pub struct QuestionData {
 }
 
 impl QuestionData {
-    fn new(
+    pub fn new(
         id: Uuid,
+        course_key: String,
         text: String,
         image_file_name: Option<PathBuf>,
         question_options: Vec<QuestionOptionData>,
@@ -263,7 +188,7 @@ impl QuestionData {
     ) -> Self {
         Self {
             id,
-            course_key: None,
+            course_key,
             evaluation,
             source,
             asked_at,
@@ -362,56 +287,16 @@ impl QuestionData {
         Ok(())
     }
 
-    async fn format(&mut self, images_path: PathBuf) -> Result<()> {
+    fn format(&mut self) {
         self.text = format_text(&self.text);
-
-        self.format_image(images_path).await?;
 
         for question_option in self.question_options.iter_mut() {
             question_option.format();
         }
-
-        Ok(())
-    }
-
-    fn set_data(&mut self, course_key: String) {
-        self.set_course_key(course_key);
-
-        for question_option in self.question_options.iter_mut() {
-            question_option.set_data(self.id);
-        }
-
-        self.set_hash();
     }
 
     pub fn full_evaluation_key(&self) -> String {
-        CourseEvaluationData::do_full_key(self.course_key(), &self.evaluation)
-    }
-}
-
-impl CourseAssociated for QuestionData {
-    fn course_key(&self) -> &str {
-        self.course_key
-            .as_ref()
-            .expect("course key not set in question")
-    }
-
-    fn set_course_key(&mut self, course_key: String) {
-        self.course_key.replace(course_key);
-    }
-}
-
-impl WithImage for QuestionData {
-    fn current_image_file_name(&self) -> Option<&PathBuf> {
-        self.image_file_name.as_ref()
-    }
-
-    fn canonical_image_file_name(&self) -> String {
-        self.id.to_string()
-    }
-
-    fn replace_image_file_name(&mut self, new_file_name: PathBuf) {
-        self.image_file_name.replace(new_file_name);
+        CourseEvaluationData::do_full_key(&self.course_key, &self.evaluation)
     }
 }
 
@@ -421,7 +306,7 @@ impl Hashable for QuestionData {
 
         bytes.extend(self.id.as_bytes());
 
-        bytes.extend(self.course_key().as_bytes());
+        bytes.extend(self.course_key.as_bytes());
         bytes.extend(self.text.as_bytes());
 
         if let Some(image_file_name) = &self.image_file_name {
@@ -445,23 +330,11 @@ impl Hashable for QuestionData {
     }
 
     fn set_hash(&mut self) {
+        for question_option in &mut self.question_options {
+            question_option.set_hash();
+        }
+
         self.hash = self.hash_data();
-    }
-}
-
-impl From<RawQuestionData> for QuestionData {
-    fn from(raw: RawQuestionData) -> Self {
-        let options = raw.options.into_iter().map(Into::into).collect();
-
-        Self::new(
-            raw.id.unwrap_or_else(|| Uuid::new_v4()),
-            raw.text,
-            raw.image,
-            options,
-            raw.evaluation,
-            raw.source,
-            raw.asked_at,
-        )
     }
 }
 
@@ -469,7 +342,7 @@ impl From<RawQuestionData> for QuestionData {
 pub struct QuestionOptionData {
     pub id: Uuid,
 
-    pub question_id: Option<Uuid>,
+    pub question_id: Uuid,
     pub text: String,
     pub correct: bool,
     pub explanation: Option<String>,
@@ -478,10 +351,16 @@ pub struct QuestionOptionData {
 }
 
 impl QuestionOptionData {
-    fn new(id: Uuid, text: String, correct: bool, explanation: Option<String>) -> Self {
+    pub fn new(
+        id: Uuid,
+        question_id: Uuid,
+        text: String,
+        correct: bool,
+        explanation: Option<String>,
+    ) -> Self {
         Self {
             id,
-            question_id: None,
+            question_id,
             text,
             correct,
             explanation,
@@ -507,11 +386,6 @@ impl QuestionOptionData {
             self.text.push(PERIOD);
         }
     }
-
-    pub fn set_data(&mut self, question_id: Uuid) {
-        self.question_id = Some(question_id);
-        self.set_hash();
-    }
 }
 
 impl Hashable for QuestionOptionData {
@@ -519,12 +393,7 @@ impl Hashable for QuestionOptionData {
         let mut bytes = vec![];
 
         bytes.extend(self.id.as_bytes());
-        bytes.extend(
-            self.question_id
-                .as_ref()
-                .expect("question ID not set in question option")
-                .as_bytes(),
-        );
+        bytes.extend(self.question_id.as_bytes());
         bytes.extend(self.text.as_bytes());
         bytes.extend(&[self.correct as u8]);
 
@@ -540,62 +409,34 @@ impl Hashable for QuestionOptionData {
     }
 }
 
-impl From<RawQuestionOptionData> for QuestionOptionData {
-    fn from(raw: RawQuestionOptionData) -> Self {
-        Self::new(
-            raw.id.unwrap_or_else(|| Uuid::new_v4()),
-            raw.text,
-            raw.correct.unwrap_or(false),
-            raw.explanation,
-        )
-    }
-}
-
 #[derive(Serialize, Deserialize, PartialEq, Hash, Eq, Clone, Debug)]
 pub struct CourseEvaluationData {
     pub key: String,
 
-    pub course_key: Option<String>,
+    pub course_key: String,
     pub name: String,
-    pub order: Option<i16>,
+    pub order: i16,
 
     pub hash: String,
 }
 
 impl CourseEvaluationData {
-    pub fn new(raw: RawCourseEvaluationData) -> Self {
+    pub fn new(key: String, course_key: String, name: String, order: i16) -> Self {
         Self {
-            key: raw.key,
-            course_key: None,
-            name: raw.name,
-            order: None,
+            key,
+            course_key,
+            name,
+            order,
             hash: Default::default(),
         }
     }
 
     pub fn full_key(&self) -> String {
-        Self::do_full_key(self.course_key(), &self.key)
+        Self::do_full_key(&self.course_key, &self.key)
     }
 
     pub fn do_full_key(course_key: &str, key: &str) -> String {
         format!("{}{COURSE_EVALUATION_KEY_SEPARATOR}{}", course_key, key)
-    }
-
-    fn set_data(&mut self, course_key: String) {
-        self.set_course_key(course_key);
-        self.set_hash();
-    }
-}
-
-impl CourseAssociated for CourseEvaluationData {
-    fn course_key(&self) -> &str {
-        self.course_key
-            .as_ref()
-            .expect("course key not set in course evaluation")
-    }
-
-    fn set_course_key(&mut self, course_key: String) {
-        self.course_key.replace(course_key);
     }
 }
 
@@ -603,24 +444,15 @@ impl Hashable for CourseEvaluationData {
     fn hashable_data(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
-        bytes.extend(self.course_key().as_bytes());
+        bytes.extend(self.course_key.as_bytes());
         bytes.extend(self.name.as_bytes());
-
-        if let Some(order) = self.order {
-            bytes.extend(&order.to_be_bytes());
-        }
+        bytes.extend(self.order.to_be_bytes());
 
         bytes
     }
 
     fn set_hash(&mut self) {
         self.hash = self.hash_data();
-    }
-}
-
-impl From<RawCourseEvaluationData> for CourseEvaluationData {
-    fn from(raw: RawCourseEvaluationData) -> Self {
-        Self::new(raw)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod data;
 mod helpers;
-mod raw_data;
 mod sync;
 mod traits;
 
 pub use data::*;
 pub use helpers::*;
-pub use raw_data::*;
 pub use sync::*;
 pub use traits::*;
