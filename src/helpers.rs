7e9dabf332d5b9702c14d8b97d99743ba35d444e
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2023-02-27 01:15:15 -0300

    add space before percent sign

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,10 +3,14 @@ use std::path::Path;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
+const UNITS_TO_SEPARATE: [&str; 1] = ["%"];
+
 static WHITESPACE_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\s\s+").unwrap());
 static WHITESPACE_BEFORE_END_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"\s(\.|:|\?)$").unwrap());
 static DOUBLE_QUOTE_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[“”]").unwrap());
+static SPACE_BEFORE_PERCENT_SIGN_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(&format!(r"(\d)({})", UNITS_TO_SEPARATE.join("|"))).unwrap());
 
 pub fn format_text(text: &str) -> String {
     let mut formatted = text.trim().to_owned();
@@ -14,6 +18,9 @@ pub fn format_text(text: &str) -> String {
     formatted = WHITESPACE_REGEX.replace_all(&formatted, " ").into();
     formatted = WHITESPACE_BEFORE_END_REGEX.replace(&formatted, "$1").into();
     formatted = DOUBLE_QUOTE_REGEX.replace_all(&formatted, "\"").into();
+    formatted = SPACE_BEFORE_PERCENT_SIGN_REGEX
+        .replace_all(&formatted, "$1 $2")
+        .into();
 
     formatted
 }
@@ -38,6 +45,9 @@ mod tests {
 
     #[test]
     fn test_format_text() {
-        assert_eq!(format_text(" test  “text” . "), "test \"text\".");
+        assert_eq!(
+            format_text(" test  “text” 12.34% . "),
+            "test \"text\" 12.34 %."
+        );
     }
 }
